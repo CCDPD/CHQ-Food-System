8b5325c7f82324d3e7b6e069c70b38d208c67cea
Cullen <CullenB@co.chautauqua.ny.us>
2020-02-12 10:38:45 -0500

    set sidebar color theme, load geojson

--- map.js
+++ map.js
@@ -6,17 +6,5 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-var coll = document.getElementsByClassName("collapsible");
-var i;
-
-for (i = 0; i < coll.length; i++) {
-  coll[i].addEventListener("click", function() {
-    this.classList.toggle("active");
-    var content = this.nextElementSibling;
-    if (content.style.display === "block") {
-      content.style.display = "none";
-    } else {
-      content.style.display = "block";
-    }
-  });
-}
+//geocoded addresses
+var geoJSON = L.geoJSON(points).addTo(map);
