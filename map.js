50239f39e86b9eef148da60bafed4e9c31e760e5
bren96 <bcullen@Brendans-MacBook-Air-2.local>
2020-02-16 10:28:45 -0500

    add ESRI leaflet geocoder

--- map.js
+++ map.js
@@ -5,6 +5,18 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
+// ESRI Geocoder
+var searchControl = L.esri.Geocoding.geosearch().addTo(map);
+
+var results = L.layerGroup().addTo(map);
+
+searchControl.on('results', function (data) {
+  results.clearLayers();
+  for (var i = data.results.length - 1; i >= 0; i--) {
+    results.addLayer(L.marker(data.results[i].latlng));
+  }
+});
+
 
 //geocoded addresses
 
