4fc834aa4d1c3caa20619d1647e9e1068f4bde81
Cullen <CullenB@co.chautauqua.ny.us>
2020-03-05 10:13:32 -0500

    add contact us info w/ links

--- page/page.js
+++ page/page.js
@@ -65,10 +65,15 @@ add_resources_content.className = "content";
 add_resources_content.style = "display: block;";
 add_resources_content.innerHTML = (
   "<ul style:'list-style-type: none;'>" +
-  "<li><a href='http://37.60.236.155/~planning/' class='add_resources'><span class='mdi mdi-web'></span></a>Planning chautauqua Website</li>" +
-  "<li><a href='mailto:kellerm@co.chautauqua.ny.us' class='add_resources'><span class='mdi mdi-email-newsletter'></span></a>KellerM@co.chautauqua.ny.us</li>" +
-  "<li><a href='tel:7167534066' class='add_resources'><span class='mdi mdi-phone'></span></a>(716) 753 4066</li>" +
-  "<li><a href='https://github.com/bren96/CHQ-Food-System' class='add_resources'><span class='mdi mdi-github-circle'></span></a>Github Project Repository</li>" +
+  "<li><a href='http://37.60.236.155/~planning/' class='add_resources'><span class='mdi mdi-web'></span></a>" +
+  "<a href='https://www.google.com/maps/place/Chautauqua+County+Department+of+Planning/@42.3218653,-79.5806164,17z/data=!4m13!1m7!3m6!1s0x882d461215a00489:0x519024237137596a!2s2+S+Portage+St,+Westfield,+NY+14787!3b1!8m2!3d42.3218614!4d-79.5784277!3m4!1s0x882d47348b60ada9:0xc6f49db434674350!8m2!3d42.3218614!4d-79.5784277' class='add_resources'><span class='mdi mdi-map-marker'></span></a>" +
+  "<a href='mailto:kellerm@co.chautauqua.ny.us' class='add_resources'><span class='mdi mdi-email'></span></a>" +
+  "<a href='tel:7167534066' class='add_resources'><span class='mdi mdi-phone'></span></a>" +
+  "<a href='https://github.com/bren96/CHQ-Food-System' class='add_resources'><span class='mdi mdi-github-circle'></span></a></li>" +
+  "<li>Chautauqua County Division of Planning & Community Development</li>" +
+  "<li>2 South Portage St., Westfield NY 14787</li>" +
+  "<li>(716) 753 4066 </li>" +
+  "<li>KellerM@co.chautauqua.ny.us</li>" +
   "</ul>"
 );
 sidebar.appendChild(add_resources_content);
