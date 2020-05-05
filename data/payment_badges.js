a758f62666694a8b511e47debfaaf2e50eec72f8
bren96 <brendancullen996@gmail.com>
2020-05-05 13:38:26 -0400

    add title tag to payment badges

--- data/payment_badges.js
+++ data/payment_badges.js
@@ -1,22 +1,22 @@
 var payment_badges = {
   "1": {
     payment_name: "Cash Only" ,
-    payment_html: "<img src='./img/Cash Only Badge.svg' id='badges' alt='Cash Only'>",
+    payment_html: "<img src='./img/Cash Only Badge.svg' id='badges' alt='Cash Only' title='Cash Only'>",
   },
   "2": {
     payment_name: "Double Up Food Bucks",
-    payment_html: "<img src='./img/DoubleUpFoodBucks.png' id='badges' alt='Double Up Food Bucks'>",
+    payment_html: "<img src='./img/DoubleUpFoodBucks.png' id='badges' alt='Double Up Food Bucks' title='Double Up Food Bucks'>",
   },
   "3": {
     payment_name: "SNAP",
-    payment_html: "<img src='./img/Supplemental_Nutrition_Assistance_Program_logo.svg' id='badges' alt='SNAP'>",
+    payment_html: "<img src='./img/Supplemental_Nutrition_Assistance_Program_logo.svg' id='badges' alt='SNAP' title='SNAP'>",
   },
   "4": {
     payment_name: "WIC",
-    payment_html: "<img src='./img/wicnystate.jpg' id='badges' alt='WIC'>",
+    payment_html: "<img src='./img/wicnystate.jpg' id='badges' alt='WIC' title='SNAP'>",
   },
   "5": {
     payment_name: "Shoptauqua Gift Card",
-    payment_html: "<img src='./img/shoptauqua-gift-card-image.jpg' id='badges' style='width: 100px' alt='Shoptauqua Gift Card'>",
+    payment_html: "<img src='./img/shoptauqua-gift-card-image.jpg' id='badges' title='Shoptqauqua Gift Card' style='width: 50px' alt='Shoptauqua Gift Card'>",
   }
 };
